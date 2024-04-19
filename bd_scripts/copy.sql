8f5c29e209dcae305ff0abbfe4bd13b89ad65d14
rodrigovasdev <146775218+rodrigovasdev@users.noreply.github.com>
2024-04-19 03:23:00 -0400

    Update copy.sql

--- bd_scripts/copy.sql
+++ bd_scripts/copy.sql
@@ -1,3 +1,3 @@
-\copy nombre_tabla (posicion, numero, piloto, auto, vueltas, tiempo, pts, year, grand_prix, detalle, codigo_conductor)
+COPY nombre_tabla (posicion, numero, piloto, auto, vueltas, tiempo, pts, year, grand_prix, detalle, codigo_conductor)
 FROM 'RUTA\f1list\bd_scripts\sprint_results.csv'
 WITH (FORMAT csv, DELIMITER ',', HEADER true);
